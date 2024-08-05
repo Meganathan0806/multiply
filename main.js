bf91821138caf8dbf820f1a3692c5b749f2c60f1
Meganathan <bmeganathan0806@gmail.com>
2024-08-06 01:15:31 +0530

    5

---
+++ main.js
@@ -0,0 +1,5 @@
+function multi(){
+    var a=document.getElementById('num1').value;
+    var b=document.getElementById('num2').value;
+    document.getElementById('result').innerHTML+=parseInt(a)*parseInt(b);    
+}
